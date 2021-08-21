b9d1846841ab0577e4e3d59f4a58e8616b5bd4d2
Aaron Chan <chanaaron32@gmail.com>
2021-08-20 18:00:34 -0700

    Removing Windows Policy

--- asyncGoogleTrends.py
+++ asyncGoogleTrends.py
@@ -1,8 +1,6 @@
 from pytrendsasync.request import TrendReq
 from datetime import date
-import time
-import asyncio
-from multiprocessing import Pool
+import time, sys, asyncio
 
 
 slangTrend = TrendReq(timeout=(10, 25), retries=2, backoff_factor=0.1)
@@ -12,16 +10,15 @@ property = ""
 timeframe = "today 5-y"
 # testData2 = ["Poggers", "booba", "deez nuts", "monkas", "thicc", "cringe", "skinny legend", "brx", "bru", "leafy", "nightblue", "normie", "hello", "water"]
 
+if sys.version_info[0] == 3 and sys.version_info[1] >= 8 and sys.platform.startswith('win'):
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
 
 async def get_Slang(wordList):
-    # try :
     start_time = time.time()
     responses = await asyncio.gather(*[relatedSlang(kw) for kw in wordList])
     print("get_Slang --- %s seconds ---" % (time.time() - start_time))
     return list(filter(None, responses))
-    # except:
-    #     print("uh oh stinky")
-    #     return []
 
 
 async def relatedSlang(keyword):
@@ -33,10 +30,3 @@ async def relatedSlang(keyword):
             return keyword
         return None 
     return None
-
-
-# asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-
-# print(asyncio.run(get_Slang(testData2)))
-
-# print("--- %s seconds ---" % (time.time() - start_time))
